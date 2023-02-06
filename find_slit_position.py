5ffc45f40b0cea1e7277cbf80b9736a4f5a5b4b8
Vsevolod Lander <sevalander@gmail.com>
2023-02-06 23:37:19 +0400

    Adapt find_slit_position for SCORPIO data and rotated images

--- find_slit_position.py
+++ find_slit_position.py
@@ -10,6 +10,7 @@ from scipy import ndimage
 from scipy import optimize
 import astropy.units as u
 from astropy.visualization import simple_norm
+from astropy.coordinates import FK5
 
 
 def myinterp(x_new, x, f):
@@ -30,7 +31,7 @@ def correlation(vec1, vec2):
 
 def Qfunc3(xys0, img, slit0, imgscale, wy=1.5, crpix2=256, spec_x=None,
            verbose=False):
-    # imgscale = np.abs(imgscale)
+    # Slit is horizontal on image
     wy = np.abs(0.5 * wy / imgscale)
     x0, y0, slitscale = xys0
     # slitscale *= np.sign(imgscale)
@@ -39,16 +40,8 @@ def Qfunc3(xys0, img, slit0, imgscale, wy=1.5, crpix2=256, spec_x=None,
         spec_x = np.arange(len(slit))
 
     img_y = np.arange(len(img)).astype(float)
-    # print(img)
-    # print(y0, wy)
-    # print(img.T)
     img_y_des = np.arange(y0 - wy, y0 + wy, 0.1)
-    # print(img_y_des)
-    # print(img_y)
-    # print(img.T)
     img_reg = np.array([np.interp(img_y_des, img_y, x) for x in img.T]).T
-    # plt.imshow(img_reg)
-    # plt.show()
 
     # print('here')
     # print(img_reg[10])
@@ -65,6 +58,7 @@ def Qfunc3(xys0, img, slit0, imgscale, wy=1.5, crpix2=256, spec_x=None,
     # print(img_x)
 
     img_reg = myinterp(slit_x, img_x, img_reg)
+    img_reg = img_reg - img_reg.min()
     # print(img_reg)
     img_reg = norm_vector(img_reg)
     if verbose:
@@ -134,39 +128,75 @@ def myrot(x, y, rot, cent=[0, 0]):
     return [xnew, ynew]
 
 
+def get_img_scale(slit_cent, wcs, angle, center):
+    x = np.arange(-50, 50) + slit_cent[0]
+    y = np.ones(100) * slit_cent[1]
+    line = np.array(myrot(x, y, -angle, center))
+    line = [wcs.pixel_to_world(i, j) for i, j in line.T]
+    d_array = []
+    for i, p in enumerate(line):
+        if i != 0:
+            s = line[i].separation(line[i - 1]) / u.deg
+            s = s * 3600
+            d_array.append(s)
+    sep = round(np.mean(d_array), 5)
+    return sep
+
+
+def get_img_PA(wcs):
+    pix1 = wcs.pixel_to_world(0, 0)
+    pix2 = wcs.pixel_to_world(0, 1)
+    pa = pix1.position_angle(pix2)
+    return(pa.deg)
+
+
 def find_slit_position(image, spec, speclim):
-    spec_center = SkyCoord(spec.header['RA'], spec.header['DEC'],
-                           unit=(u.hourangle, u.deg))
+    if 'EPOCH' in spec.header:
+        equinox = "J" + str(spec.header['EPOCH'])
+        spec_center = SkyCoord(spec.header['RA'], spec.header['DEC'],
+                               unit=(u.hourangle, u.deg),
+                               frame=FK5(equinox=equinox))
+    else:
+        spec_center = SkyCoord(spec.header['RA'], spec.header['DEC'],
+                               unit=(u.hourangle, u.deg))
     wcs = WCS(image.header)
+    print(wcs)
+    print(spec_center)
     xy_cent = [int(t) for t in wcs.world_to_pixel(spec_center)]
 
-    imgscale = image.header['CD1_1'] * 3600
+    imsc_sgn = np.sign(image.header['CD1_1'] * 3600)
+    print('Image scale ', imsc_sgn)
+
+    imgPA = get_img_PA(wcs)
+    print('Image PA ', imgPA)
+
     specscale = spec.header['CDELT2']
 
     if 'CRPIX2' in spec.header:
         crpix2 = spec.header['CRPIX2']
     else:
-        crpix2 = 1
+        crpix2 = spec.header['NAXIS2'] // 2
 
     if 'POSANG' in spec.header:
         # TDS
         PA = spec.header['POSANG']
     elif 'PARANGLE' in spec.header and 'ROTANGLE' in spec.header:
         # SCORPIO
-        PA = spec.header['PARANGLE'] - spec.header['ROTANGLE'] + 132.5
+        PA = spec.header['PARANGLE'] - spec.header['ROTANGLE'] + 132.5 + 90
     else:
         PA = 0
+
+    if PA >= 360:
+        PA -= 360
+
+    rotangle = PA - imgPA - 90
+    if np.abs(rotangle) < 1:
+        rotangle = 0
 
     if 'SLIT' in spec.header:
         wy = parse_tds_slit(spec.header['SLIT'])
     else:
         wy = 1.0
-
-    print('Spectrum CRPPIX2 ', crpix2)
-    print('Spectrum scale ', specscale)
-    print('Image scale ', imgscale)
-    print('PA ', PA)
-    print('Slit width ', wy)
 
     spec_data = spec.data[speclim]
     spec_x = np.arange(spec.header['NAXIS2'])[speclim]
@@ -175,8 +205,17 @@ def find_slit_position(image, spec, speclim):
 
     img = image.data
     center_image = [len(img[0]) / 2, len(img) / 2]
-    xy_center = myrot(*xy_cent, PA - 90, center_image)
-    img = ndimage.rotate(img, PA - 90, reshape=False, mode='nearest')
+    xy_center = myrot(*xy_cent, rotangle, center_image)
+    img = ndimage.rotate(img, rotangle, reshape=False, mode='nearest')
+    print("non-rotated center", xy_cent)
+
+    imgscale = get_img_scale(xy_center, wcs, rotangle, center_image) * imsc_sgn
+
+    print('Spectrum CRPPIX2 ', crpix2)
+    print('Spectrum scale ', specscale)
+    print('Image scale ', imgscale)
+    print('PA ', PA)
+    print('Slit width ', wy)
 
     dx, dy = dxdy_setup(img, F_slit, imgscale, specscale, xy_center, wy, crpix2,
                         spec_x)
@@ -197,13 +236,16 @@ def find_slit_position(image, spec, speclim):
     print(xys0)
     print(Qfunc3(xys0.x, img, F_slit, imgscale, wy, crpix2, spec_x,
                  verbose=True))
-    radec = myrot(*xys0.x[:2], -(PA - 90), center_image)
+    radec = myrot(*xys0.x[:2], -rotangle, center_image)
     radec = wcs.pixel_to_world(*radec)
 
     spec_header_new = spec.header.copy()
     spec_header_new['CDELT2'] = xys0.x[2]
     spec_header_new['RA'] = radec.ra.to_string(sep=':', unit=u.hourangle)
     spec_header_new['DEC'] = radec.dec.to_string(sep=':')
+    spec_header_new['CRPIX2'] = crpix2
+    if 'EPOCH' in spec_header_new:
+        spec_header_new['EPOCH'] = 2000.0
     return(spec_header_new)
 
 
@@ -227,11 +269,11 @@ def main(args=None):
     if pargs.ymin is not None:
         ymin = int(pargs.ymin)
     else:
-        ymin = 50
+        ymin = None
     if pargs.ymax is not None:
         ymax = int(pargs.ymax)
     else:
-        ymax = 450
+        ymax = None
     ylim = slice(ymin, ymax)
 
     hdr_new = find_slit_position(image, spectrum, ylim)
